d400026484d30676ffd76a6bfc5aee276283479e
DEEPAK-crypto <deepsdeep45@gmail.com>
2020-05-24 23:54:31 +0530

    added new form

--- app.js
+++ app.js
@@ -33,6 +33,19 @@ app.get('/blogs', function(req, res) {
         else
             res.render("index", { blogs: blogs });
     })
+});
+
+app.get("/blogs/new", function(req, res) {
+    res.render('new');
+});
+
+app.post('/blogs', function(req, res) {
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if (err)
+            console.log(err);
+        else
+            res.redirect("/blogs");
+    })
 })
 
 app.listen(3000);
